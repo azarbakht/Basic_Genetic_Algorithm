735859ca83f4ef0e8a153422c68994d0978c09eb
Amir Azarbakht <amiazarb@gmail.com>
2014-05-17 12:48:13 -0700

    basic genetic algorithm

---
+++ gamain.m
@@ -0,0 +1,111 @@
+populationSize = 100; % Even population size assumed in this program.
+numberOfGenes = 50;
+crossoverProbability = 0.8;
+mutationProbability = 0.01;
+tournamentSelectionParameter = 0.75;
+variableRange = 5.0;
+numberOfGenerations = 200;
+fitness = zeros(populationSize,1);
+nEvaluationsPerformed = 0;
+
+fitnessFigureHandle = figure;
+hold on;
+set(fitnessFigureHandle, 'Position', [50,50,500,200]);
+set(fitnessFigureHandle, 'DoubleBuffer','on');
+axis([1 numberOfGenerations 2.5 3]);
+bestPlotHandle = plot(1:numberOfGenerations,zeros(1,numberOfGenerations));
+textHandle = text(30,2.6,sprintf('Best: %4.3f', 0.0));
+hold off;
+drawnow;
+
+surfaceFigureHandle = figure;
+hold on;
+set(surfaceFigureHandle, 'DoubleBuffer','on');
+delta = 0.1;
+limit = fix(2*variableRange/delta) + 1;
+[xValues,yValues] = meshgrid(-variableRange:delta:variableRange,-variableRange:delta:variableRange);
+zValues = zeros(limit,limit);
+for j = 1:limit
+for k = 1:limit
+zValues(j,k) = EvaluateIndividual([xValues(j,k) yValues(j,k)]);
+end
+end
+surfl(xValues,yValues,zValues)
+colormap gray;
+shading interp;
+view([-7 -9 10]);
+decodedPopulation = zeros(populationSize,2);
+populationPlotHandle = plot3(decodedPopulation(:,1), decodedPopulation(:,2), fitness(:),'kp');
+hold off;
+drawnow;
+
+population = InitializePopulation(populationSize, numberOfGenes);
+for iGeneration = 1:numberOfGenerations
+maximumFitness = 0.0; % Assumes non-negative fitness values!
+for i = 1:populationSize
+chromosome = population(i,:);
+    % Note! Minimization:
+x = DecodeChromosome(chromosome, variableRange);
+decodedPopulation(i,:) = x; % The new statement
+fitness(i) = 1.0/EvaluateIndividual(x);
+if (fitness(i) > maximumFitness)
+maximumFitness = fitness(i);
+bestIndividualIndex = i;
+xBest = x;
+end
+end
+maximumFitness, xBest 
+% Printout
+
+
+
+ 
+
+ tempPopulation = population;
+for i = 1:2:populationSize
+i1 = TournamentSelect(fitness,tournamentSelectionParameter);
+i2 = TournamentSelect(fitness,tournamentSelectionParameter);
+chromosome1 = population(i1,:);
+chromosome2 = population(i2,:);
+r = rand;
+if (r < crossoverProbability)
+newChromosomePair = Cross(chromosome1,chromosome2);
+tempPopulation(i,:) = newChromosomePair(1,:);
+tempPopulation(i+1,:) = newChromosomePair(2,:);
+else
+tempPopulation(i,:) = chromosome1;
+tempPopulation(i+1,:) = chromosome2;
+end
+end % Loop over population
+for i = 1:populationSize
+originalChromosome = tempPopulation(i,:);
+mutatedChromosome = Mutate(originalChromosome,mutationProbability);
+tempPopulation(i,:) = mutatedChromosome;
+end
+tempPopulation(1,:) = population(bestIndividualIndex,:);
+population = tempPopulation;
+
+plotvector = get(bestPlotHandle,'YData');
+plotvector(iGeneration) = maximumFitness;
+set(bestPlotHandle,'YData',plotvector);
+set(textHandle,'String',sprintf('best: %4.3f',maximumFitness));
+
+set(populationPlotHandle,'XData',decodedPopulation(:,1),'YData', decodedPopulation(:,2),'ZData',fitness(:));
+
+drawnow;
+
+end % Loop over generations
+maximumFitness, xBest
+
+
+
+
+
+
+
+
+
+
+
+
+
